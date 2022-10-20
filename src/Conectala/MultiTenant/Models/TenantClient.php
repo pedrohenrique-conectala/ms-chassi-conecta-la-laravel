efe0b9680106ab801d50bfcc5b9bd1136ba39851
Pedro Henrique <pedrohenrique@conectala.com.br>
2022-10-20 17:36:15 -0300

    update TenantClient.php

--- src/Conectala/MultiTenant/Models/TenantClient.php
+++ src/Conectala/MultiTenant/Models/TenantClient.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Conectala\MultiTenant\Migration\Models;
+namespace Conectala\MultiTenant\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
