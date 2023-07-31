36d60cb29091343ac84a71d1c6d2bae8675ba33d
pedrohenrique-conectala <108018163+pedrohenrique-conectala@users.noreply.github.com>
2023-07-31 16:22:33 -0300

    Update command.sh

--- src/Conectala/Script/command.sh
+++ src/Conectala/Script/command.sh
@@ -10,12 +10,10 @@ MS_NAME=$1
 
 MAIN_PATH=./
 APP_PATH=$MAIN_PATH/app
-LIBRARY_PATH=$MAIN_PATH/vendor/pedrohenrique-conectala/ms-chassi-conecta-la-laravel/src/Conectala
 
 #-----------------ESTRUTURA CONDICIONAL--------------------
 if [ -n "$MS_NAME" ]; then
-  cp "$LIBRARY_PATH/MultiTenant/Routes/api.php" "$MAIN_PATH/routes/api.php"
-  sed -i "s/->prefix('api')/->prefix('$MS_NAME/api')/g" "$APP_PATH/Providers/RouteServiceProvider.php"
+  sed -i "s@prefix('api')@prefix('$MS_NAME/api')@g" "$APP_PATH/Providers/RouteServiceProvider.php"
 fi;
 
 exit 1;
