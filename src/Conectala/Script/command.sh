abd40bf073747b0a416e00ffa67a40f3d37e76b9
pedrohenrique-conectala <108018163+pedrohenrique-conectala@users.noreply.github.com>
2023-07-31 15:35:30 -0300

    Update command.sh

--- src/Conectala/Script/command.sh
+++ src/Conectala/Script/command.sh
@@ -12,37 +12,10 @@ MAIN_PATH=./
 APP_PATH=$MAIN_PATH/app
 LIBRARY_PATH=$MAIN_PATH/vendor/pedrohenrique-conectala/ms-chassi-conecta-la-laravel/src/Conectala
 
-DATABASE_PATH=$MAIN_PATH/database
-
-MIGRATION_PATH_SYSTEM=$DATABASE_PATH/migrations/system
-MIGRATION_PATH_TENANT=$DATABASE_PATH/migrations/tenant
-
-CONFIG_PATH_LUMEN=$MAIN_PATH/config
-
 #-----------------ESTRUTURA CONDICIONAL--------------------
-
-if [ ! -e "$MIGRATION_PATH_SYSTEM" ]; then
-  mkdir $MIGRATION_PATH_SYSTEM
-fi;
-
-if [ ! -e "$MIGRATION_PATH_TENANT" ]; then
-  mkdir $MIGRATION_PATH_TENANT
-fi;
-
-if [ ! -e "$CONFIG_PATH_LUMEN" ]; then
-  mkdir $CONFIG_PATH_LUMEN
-fi;
-
-if [ ! -e "$APP_PATH/Http/Controllers/API/V1" ]; then
-  mkdir -p "$APP_PATH/Http/Controllers/API/V1"
-fi;
-
 if [ -n "$MS_NAME" ]; then
   cp "$LIBRARY_PATH/MultiTenant/Routes/api.php" "$MAIN_PATH/routes/api.php"
-  sed -i "s/ms_creating_replace/$MS_NAME/g" "$MAIN_PATH/routes/api.php"
-  cp "$LIBRARY_PATH/MultiTenant/Commands/Copy/Kernel.php" "$APP_PATH/Console/Kernel.php";
-
-  sed -i "s/ms_creating_replace_conectala/ms_${MS_NAME}_conectala/g" "$DATABASE_PATH/seeders/TenantClientsTableSeeder.php"
+  sed -i "s/->prefix('api')/->prefix('$MS_NAME/api')/g" "$APP_PATH/Providers/RouteServiceProvider.php"
 fi;
 
 exit 1;
